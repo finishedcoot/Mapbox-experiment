c1750a7f402ec0cedb482e14847cb6a98d56c8c9
Ardshir <83403777+finishedcoot@users.noreply.github.com>
2021-10-19 23:03:43 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZmluaXNoZWRjb290IiwiYSI6ImNrdXNnYTlmZjExOWwydnF2YjY4cG81cWcifQ.S97K1xRVxHMIXMynAKJVCw";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
